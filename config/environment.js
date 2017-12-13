5a52bba53f8314e1576a1b9e714e3ff17a1ab2d1
Mario Linz <gelight@gmx.de>
2017-12-13 12:44:46 +0100

    Changed locationType for production to 'hash'

--- config/environment.js
+++ config/environment.js
@@ -60,6 +60,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
+    locationType: 'hash'
   }
 
   return ENV;
